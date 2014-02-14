f46ee7fffed5fb3ddad834469478a3c76b77363e
Daniel Kronovet <kronovet@gmail.com>
2014-02-14 14:49:55 -0500

    added db initialization to student class

--- lib/models/student.rb
+++ lib/models/student.rb
@@ -1,2 +1,10 @@
 class Student
+  
+DB = SQLite3::Database.new “students.db”
+DB.execute(“CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)”)
+
+attr_accessor :name
+
+  
+  
 end
